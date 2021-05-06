908c7f7837ce0abec63cc77c691b7a106923b7da
Diego_braz <you@example.com>
2021-05-05 23:09:18 -0300

    finalizando o adapter

--- app/src/main/java/com/example/pokedex/adapter/PokemonListAdapter.kt
+++ app/src/main/java/com/example/pokedex/adapter/PokemonListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.R
 import com.example.pokedex.api.domain.Pokemon
+import kotlinx.android.synthetic.main.pokemon_detail.view.*
 
 class PokemonListAdapter(
       private  val items : List<Pokemon>
@@ -30,6 +31,15 @@ class PokemonListAdapter(
     class Viewholder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
         fun bind (pokemon: Pokemon) {
+            itemView.cod_pokemon.text = "Nº ${pokemon.cod}"
+            itemView.name_pokemon.text = pokemon.name
+            itemView.type_name.text = pokemon.type.first().name
+            if (pokemon.type.size > 1){
+                itemView.type_second_name.text = pokemon.type[1].name
+            }
+            else{
+                itemView.type_second_name.visibility = View.GONE
+            }
 
 
         }
--- app/src/main/java/com/example/pokedex/view/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/view/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.pokedex.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.R
+import com.example.pokedex.adapter.PokemonListAdapter
+import com.example.pokedex.api.domain.Pokemon
+import com.example.pokedex.api.domain.PokemonType
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val charmander = Pokemon(
+            "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png", 1, "CHARMANDER",
+            listOf(
+                PokemonType("fire")
+            )
+        )
+        val pokemons = listOf(
+            charmander, charmander, charmander
+        )
+
+        val recyclerView = findViewById<RecyclerView>(R.id.poke_list)
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = PokemonListAdapter(pokemons)
     }
 }
